e34094e7becd3765998e5514ae0eabb1aa60a80e
Joshua Kenji <85597573+JoshuaKenji@users.noreply.github.com>
2023-09-18 10:10:36 +0800

    Add files via upload

---
+++ home_page.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html lang="en">
+
+  <head>
+    <title>Red Heart - Home Page</title>
+    <link rel="stylesheet" href="home_page_style.css"/>
+  </head>
+  
+  <body>
+    <div class="main" id="home">
+
+        <div class="navbar">
+            <div class="icon">
+                <h2 class="logo"><a href="home_page.html" style="color: #DC143C;text-decoration: none;">Red Heart</a></h2>
+            </div>
+
+            <div class="menu">                    
+                <ul>
+                    <li><a href="home_page.html#home">HOME</a></li>
+                    <li><a href="home_page.html#about_us">ABOUT US</a></li>
+                    <li><a href="home_page.html#contact">CONTACT</a></li>
+                    <li><a href="Doctor/login_page-D.php">DOCTOR</a></li>
+                    <li><a href="/CODE/Admin/login_page-A.php">ADMIN</a></li>
+                </ul>
+            </div>
+<!-- 
+            <div class="login-section"><a href="#">
+                <img class="login-icon" src="icons/login-icon.png">
+                <div class="login">Login</div>
+                </a>
+            </div> -->
+        </div>
+
+        
+        <div class="home">
+
+            <h1>You Donate<br>
+            We Receive<br>
+            We Save</h1>
+
+            <p class="par1">We are Red Heart Donations, a volunteer-based organization that<br>helps the blood donation around West Malaysia. We appreciate all the<br>support that we get from all of the Donors, voluntary Doctors and Staff.<br>If you would love to support or in need of supply, you may sign in!</p>
+        
+            <button class="ju"><a href="LoginPage/login_page.php">JOIN US</a></button>
+
+        </div>
+    </div>
+
+    <div class="white_break"></div>
+
+    <div class="main2" id="about_us">
+
+        <div class="AboutUs">
+            <br><br>
+            <h1>About Us</h1>
+            <p class="par2">We are a volunteer-based organization in West Malaysia that helps<br>
+            the blood donation process and it's distribution to the blood banks.<br>
+            Red Heart has been operating in West Malaysia since July 2015 and have<br>
+            been handling the donation of blood up to 5 million blood bags with the<br>
+            help of 200 thousand volunteers throughout the years.</p>  
+        </div>
+    </div>
+
+    <div class="white_break2"></div>
+
+    <div class="main3" id="contact">
+        <div class="contact">
+
+            <br><br>
+
+            <h1>Contact Us</h1>
+            <p class="par3">
+            <span style="font-weight: bold; color: #f54d4d; text-shadow: 0.5px 0.5px 1px #000000;">CONTACT NUMBER &nbsp; </span> +60XX-XXX-XXXX
+            <br>
+            <span style="font-weight: bold; color: #f54d4d; text-shadow: 0.5px 0.5px 1px #000000;">EMAIL SUPPORT &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; </span> RedHeartService@support.rh.my
+            <br>
+            <span style="font-weight: bold; color: #f54d4d; text-shadow: 0.5px 0.5px 1px #000000;">HQ ADDRESS &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;</span> Jalan Lorem, Taman Park Zero, 62025 Kuala Lumpur, Malaysia
+            </p>  
+        </div>
+    </div>
+
+    <div class="white_break"></div>
+
+  </body>
+</html>
